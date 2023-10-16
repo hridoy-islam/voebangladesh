435cf19bfb80bd19eae3aa37c9693f24d712cf43
Mushfik Ahmed <ahmed.mushfik16381@gmail.com>
2023-10-16 13:44:21 +0600

    updatescroll remove

--- vue/src/views/Updates.vue
+++ vue/src/views/Updates.vue
@@ -1,14 +1,14 @@
 <template lang="">
     <div>
         <PageTitle title="Our Updates" :arrows="true" :dots="true"/>
-       <UpdateScroll/>
+       <!-- <UpdateScroll/> -->
        <UpdateSponsor/>
     </div>
 </template>
 <script setup>
 import PageTitle from '../components/PageTitle.vue';
 
-import UpdateScroll from '../components/UpdateScroll.vue';
+// import UpdateScroll from '../components/UpdateScroll.vue';
 import UpdateSponsor from '../components/UpdateSponsor.vue';
 
 
